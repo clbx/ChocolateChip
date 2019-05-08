8940f0a2fc40858a9032c33922fac33e8dac7189
Clay Buxton <clay@clbx.io>
2019-05-07 21:38:32 -0400

    all opcode implemented. debugging time

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # ChocolateChip
 🍪 Round 2 With the CHIP-8 Emulator
 
-Some very useful resources:
+
+
+# Some very useful resources:
 
 [Chip 8's Wikipedia Page](https://en.wikipedia.org/wiki/CHIP-8)
 
 [Cowgod's Chip 8 Technical Reference](http://devernay.free.fr/hacks/chip8/C8TECH10.HTM)
 
 
+# Improved Stuff in Rewrite;
+
+* **SDL instead of SFML** I've found SDL to be much easier to use, plus it's much more widespread.
+* **Much shorter and concise operations** All the usual operands are grabbed at the beginning, makes code _much_ shorter (most of the opcodes are one-liners), consistent, and less prone to error.
+
+* **Fixed a bunch of bugs** These will be explained on later in the soon-to-be extensive README/Wiki
--- src/Chocolate.cpp
+++ src/Chocolate.cpp
@@ -136,8 +136,8 @@ void Chocolate::tick(){
  * 
  */
 void Chocolate::_00E0(){
-    memset(graphics,0,2048);            //Set graphics memory to 0
-    draw = true;                        //Tell SDL to render new screen
+    memset(graphics,0,2048);
+    draw = true;
 }
 
 /**
@@ -145,8 +145,8 @@ void Chocolate::_00E0(){
  * 
  */
 void Chocolate::_00EE(){
-    pc = pop();                         //Pop from stack to get return location
-    pcDelta = 2;                        //Go to instruction after
+    pc = pop();             
+    pcDelta = 2;               
 }
 
 /**
@@ -171,95 +171,280 @@ void Chocolate::_2NNN(){
  * 
  */
 void Chocolate::_3XNN(){
-    (V[X] == NN) ? pcDelta = 2:pcDelta=1;
+    pcDelta = (V[X] == NN) ? 2:1;
 }
 
+/**
+ * @brief Skips the next instructio if V[X] != NN
+ * 
+ */
 void Chocolate::_4XNN(){
-    (V[X] != NN) ? pcDelta = 2:pcDelta=1;
+    pcDelta = (V[X] != NN) ? 2:1;
 }
+
+
+/**
+ * @brief Skips the next instruction if V[X] == V[Y]
+ * 
+ */
 void Chocolate::_5XY0(){
-    
+    pcDelta = (V[X] == V[Y]) ? 2:1;
 }
+
+/**
+ * @brief Sets V[X] to NN
+ * 
+ */
 void Chocolate::_6XNN(){
-
+    V[X] = NN;
 }
+
+/**
+ * @brief Adds NN to V[X] (Ignores Carry Flag)
+ * 
+ */
 void Chocolate::_7XNN(){
-
+    V[X] += NN;
 }
+
+/**
+ * @brief Sets V[X] to V[Y]
+ * 
+ */
 void Chocolate::_8XY0(){
-
+    V[X] = V[Y];
 }
+
+/**
+ * @brief Sets V[X] to V[X] | V[Y]
+ * 
+ */
 void Chocolate::_8XY1(){
-
+    V[X] = V[X] | V[Y];
 }
+
+/**
+ * @brief Sets V[X] to V[X] & V[Y] 
+ * 
+ */
 void Chocolate::_8XY2(){
-
+    V[X] = V[X] & V[Y];    
 }
+
+/**
+ * @brief Set V[X] to V[X] ^ V[Y]
+ * 
+ */
 void Chocolate::_8XY3(){
-
+    V[X] = V[X] ^ V[Y];
 }
+
+/**
+ * @brief Adds V[Y] to V[X]. V[F] is set to 1 when there is a carry and 0 when there isn't
+ * 
+ */
 void Chocolate::_8XY4(){
-
+    V[0xF] = ((V[X] + V[Y]) > 0xFFFF) ? 1:0;
+    V[X] += V[Y];
 }
+
+/**
+ * @brief V[Y] is subtraced from V[X]. V[F] is set to 0 when there is a borrow and 1 where there isnt
+ * 
+ */
 void Chocolate::_8XY5(){
-
+    (V[Y] > V[X]) ? V[0xF]=0:V[0xF]=1;
+    V[X] -= V[Y];
 }
+
+/**
+ * @brief Stores the least signifigant bit of V[X] in V[F] and then shifts V[X] to the right by 1
+ * 
+ */
 void Chocolate::_8XY6(){
-
+    V[0xF] = V[X] & 0x0001; //Get least signifigant bit
+    V[X] >>= 1;                    
 }
+
+/**
+ * @brief Sets V[X] to V[Y] - V[X]. V[F] is set to 0 when there is a borrow, 1 when there isn't
+ * 
+ */
 void Chocolate::_8XY7(){
-
+    (V[Y] < V[X]) ? V[0xF]=0:V[0xF]=1;
+    V[X] = V[Y] - V[X];
 }
+
+/**
+ * @brief Stores the most signifigant bit of V[X] in V[F] then shifts V[F] to the left by 1
+ * 
+ */
 void Chocolate::_8XYE(){
-
+    V[0xF] = V[X] & 0x80; //Get most signifigant bit
+    V[X] <<= 1;
 }
+
+/**
+ * @brief Skips the next instruction if V[X] != V[Y]
+ * 
+ */
 void Chocolate::_9XY0(){
-
+    (V[X] != V[Y]) ? pcDelta =2:pcDelta=1;
 }
+
+/**
+ * @brief Sets I to the address NNN
+ * 
+ */
 void Chocolate::_ANNN(){
-
+    I = NNN;
 }
+
+/**
+ * @brief Jups to the address NNN plus V[0]
+ * 
+ */
 void Chocolate::_BNNN(){
-
+    I = NNN + V[0];
 }
+
+/**
+ * @brief Sets V[X] to the result of a bitwise and operation on a random number (Typically 0 to 255) and NN
+ * 
+ */
 void Chocolate::_CXNN(){
-
+    V[X] = (rand() % (0xFF +1)) & NN;
 }
+
+/**
+ * @brief Draws a sprite at (V[X],V[Y]) that has a width of 8 pixels and a height of N pixels
+ * Each row of 8 blices is read as a bit-coded from memory location I; I value doesn't change after 
+ * execution. As described above V[F] is set to 1 if any pixels are flipped from the set to unset when the
+ * sprite is drawn, and to 0 if that doesn't happen
+ */
 void Chocolate::_DXYN(){
 
+    uint16_t pixel;
+    
+    V[0xF] = 0;
+
+    for(int i = 0; i < N; i++){
+        pixel = memory[I+i];
+        for(int j = 0; j < 8; j++){
+            if((pixel & (0x80 >> j)) != 0){
+                if(graphics[(X + j + ((Y+ i) * 64)) ] == 1){
+                    V[0xF] = 1;
+                }
+                graphics[X + j + ((Y+i) * 64)] ^= 1;
+            }
+        }
+    }
+    draw = true;
 }
+
+
+/**
+ * @brief Skips the next instruction if the key stored in V[X] is pressed
+ * 
+ */
 void Chocolate::_EX9E(){
-
+    pcDelta = (keymap[V[X]] != 0) ? 2:1;    
 }
+
+/**
+ * @brief Skips the next instruction if the key stored in V[X] isn't pressed
+ * 
+ */
 void Chocolate::_EXA1(){
-
+    pcDelta = (keymap[V[X]] != 0) ? 2:1;
 }
+
+/**
+ * @brief Sets V[X] to the value of the delay timer
+ * 
+ */
 void Chocolate::_FX07(){
-
+    V[X] = delay;
 }
+
+/**
+ * @brief A key press is awaited, and then stored in V[X]
+ * 
+ */
 void Chocolate::_FX0A(){
+    bool keypress = false;
 
+    for(int i = 0; i < 16; i++){
+        if(keymap[i] != 0){
+            V[X] = i;
+            keypress = true;
+        }
+    }
+    if(!keypress){
+        pcDelta = 0;
+    }
 }
+
+/**
+ * @brief Sets the delay timer to V[X]
+ * 
+ */
 void Chocolate::_FX15(){
-
+    delay = V[X];    
 }
+
+/**
+ * @brief Sets the sound timer to V[X]
+ * 
+ */
 void Chocolate::_FX18(){
-
+    sound = V[X];
 }
+
+/**
+ * @brief Adds V[X] to I
+ * 
+ */
 void Chocolate::_FX1E(){
-
+    I += V[X];
 }
+
+/**
+ * @brief Sets I to the location of the sprice to the character in V[X] (0-F)
+ * 
+ */
 void Chocolate::_FX29(){
-
+    I = V[X] * 0x5;
 }
+
+/**
+ * @brief Stores the BCD respresentation of V[X]. 100's digit at I, 10's at I+1, I's at I+2
+ */
 void Chocolate::_FX33(){
-
+        memory[I] = V[X] /100;
+        memory[I+1] = (V[X] /10)%10;
+        memory[I+2] = V[X] % 10;
 }
-void Chocolate::_FX55(){
 
+/**
+ * @brief Stores V[0] to V[X] in memory starting at address I. The offset from I is increased
+ * by 1 for each value written but I itself is left unmodified
+ */
+void Chocolate::_FX55(){  
+    for(int i = 0; i <= V[X]; i++){
+        memory[I + i] = V[i];
+    }
 }
+
+/**
+ * @brief Fills V[0] to V[X] with falues from memory starting at address I. The offset from I
+ * is increased by 1 for each value written, but I itself is left unmodified 
+ * 
+ */
 void Chocolate::_FX65(){
-
+    for(int i = 0; i <= V[X]; i++){
+        V[i] = memory[I+i];
+    }
 }
 
 
